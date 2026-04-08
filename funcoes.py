24c66727050eed7e6ff44c76949cbcfd9e4dafca
Luiz Guilherme <luizgfc@al.insper.edu.br>
2026-04-08 11:42:16 -0300

    exercício 2 completo

--- funcoes.py
+++ funcoes.py
@@ -5,3 +5,16 @@ def rolar_dados(n):
         valor=random.randint(1, 6)
         dados.append(valor)
     return dados
+
+def guardar_dado(rolados, estoque, guardar):
+    lista=[]
+    rol=[]
+    i=guardar
+    estoque.append(rolados[i])
+    for d in range(len(rolados)):
+        if d!=i:
+            rol.append(rolados[d])
+
+    lista.append(rol)
+    lista.append(estoque)
+    return lista

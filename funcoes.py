39ba1dafd74f8e92d74046c581f11f8e7c8ddfb8
Luiz Guilherme <luizgfc@al.insper.edu.br>
2026-04-09 10:39:44 -0300

    exercício 3 completo

--- funcoes.py
+++ funcoes.py
@@ -1,11 +1,12 @@
 import random
+#exercício 1
 def rolar_dados(n):
     dados=[]
     for i in range(n):
         valor=random.randint(1, 6)
         dados.append(valor)
     return dados
-
+#exercício 2
 def guardar_dado(rolados, estoque, guardar):
     lista=[]
     rol=[]
@@ -18,3 +19,15 @@ def guardar_dado(rolados, estoque, guardar):
     lista.append(rol)
     lista.append(estoque)
     return lista
+#exercício 3
+def remover_dado(rolados, guardados, removido):
+    rolados.append(guardados[removido])
+    guard=[]
+    for i in range(len(guardados)):
+        if i!=removido:
+            guard.append(guardados[i])
+    lista=[]
+    lista.append(rolados)
+    lista.append(guard)
+    return lista
+
